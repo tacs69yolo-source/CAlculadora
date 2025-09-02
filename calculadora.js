62466e08a6af7d81652d59d24006bc4311a615ec
mariroxy2024@gmail.com <mariroxy2024@gmail.com>
2025-09-01 21:44:52 -0300

    se añadieron funciones

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,11 @@
 let variable=""
+let segundoNumero= ""
+let opCode=""
+
 
 function mostrarNumero(numero){
     variable+=numero;
-    document.getElementById("texto1").textContent=variable
+    document.getElementById("texto1").textContent= variable
   
 
 }
@@ -10,12 +13,46 @@ function eliminar (numero){
     document.getElementById("texto1").textContent=""
 }
 
-function suma (numero){
-    primerNumero=variable;
-    variable+=numero;
-    document.getElementById("texto1").textContent=variable
+function modo(id){
+    segundoNumero=variable;
     variable=""
+    opCode=id
+    document.getElementById("texto2").textContent= segundoNumero + id
+}
+function raiz(){
+    Math.sqrt(variable)
+    document.getElementById("texto1").textContent=  Math.sqrt (variable)
+    
+    document.getElementById("texto2").textContent= "√"+ variable
+    
+
 }
 function igual(){
-    document.getElementById("texto1").textContent=Number()
+   
+    let resultado;
+    if(opCode=="+"){
+        resultado = Number(segundoNumero) + Number(variable);
+    }
+    if(opCode=="-"){
+        resultado = segundoNumero - variable;
+    }
+    if(opCode=="*"){
+        resultado = segundoNumero * variable;
+    }
+     if(opCode==""){
+        resultado = segundoNumero * variable;
+    }
+      if(opCode=="/"){
+        //si
+        if(variable=="0")
+            //condicion verdadera
+            resultado= "no se puede devidir por 0"
+        //si no
+        else{
+            //condicion falsa
+            resultado = segundoNumero / variable;
+        }
+    }
+    document.getElementById("texto1").textContent= resultado
+
 }
