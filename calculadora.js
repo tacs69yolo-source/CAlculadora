07272756c6075ea14e2bc49dff1ee3e333909c10
mariroxy2024@gmail.com <mariroxy2024@gmail.com>
2025-09-04 21:51:23 -0300

    calculadora terminada

--- calculadora.js
+++ calculadora.js
@@ -1,18 +1,38 @@
 let variable=""
 let segundoNumero= ""
 let opCode=""
+let resultado=false;
+
 
 
 function mostrarNumero(numero){
+     if(resultado){
+        borrarTodo()
+        resultado=false
+    }
     variable+=numero;
     document.getElementById("texto1").textContent= variable
-  
-
 }
-function eliminar (numero){
-    document.getElementById("texto1").textContent=""
-}
+function borrarTodo(){
+    variable = ""
+    segundoNumero = ""
+    
+    document.getElementById("texto1").textContent= 0
+    document.getElementById("texto2").textContent= 0
 
+    
+}
+function borrarArriba(){
+    segundoNumero= ""
+    variable= variable
+    document.getElementById("texto1").textContent= 0
+}
+function eliminar (){
+    variable=variable.slice(0, -1)
+    document.getElementById("texto1").textContent= variable;
+   
+}
+hola = prompt
 function modo(id){
     segundoNumero=variable;
     variable=""
@@ -28,31 +48,47 @@ function raiz(){
 
 }
 function igual(){
-   
-    let resultado;
-    if(opCode=="+"){
-        resultado = Number(segundoNumero) + Number(variable);
-    }
-    if(opCode=="-"){
-        resultado = segundoNumero - variable;
-    }
-    if(opCode=="*"){
-        resultado = segundoNumero * variable;
-    }
-     if(opCode==""){
-        resultado = segundoNumero * variable;
-    }
-      if(opCode=="/"){
-        //si
-        if(variable=="0")
-            //condicion verdadera
-            resultado= "no se puede devidir por 0"
-        //si no
-        else{
-            //condicion falsa
-            resultado = segundoNumero / variable;
-        }
-    }
-    document.getElementById("texto1").textContent= resultado
-
+    document.getElementById("texto2").textContent = segundoNumero + opCode + variable+ "="
+    calculo()
+    document.getElementById("texto1").textContent = variable
 }
+function guaradarNumero(){
+    segundoNumero = variable
+    variable = ""
+}
+function calculo(){
+   switch (opCode){
+    case "+":
+        variable = Number (segundoNumero) + Number (variable)
+    break;
+    case "-":
+        variable = segundoNumero - variable
+    break;
+    case "/":
+        variable = dividir (segundoNumero,variable)
+    break;
+    case "x":
+        variable*=segundoNumero 
+        // numero = numero * segundoNumero
+    break;
+    case "+/-":
+        if(variable>0){
+    variable= "-" +variable
+   }else{
+    variable=variable.slice(1);
+   }
+    default:
+    
+    break;
+   }
+   document.getElementById("texto1").textContent = variable
+   }
+   function dividir(segundoNumero , variable){
+    if(variable==0){
+        return "no se puede dividir por 0"
+    }
+    return segundoNumero / variable
+   }
+   function potencia(){
+    
+   }
